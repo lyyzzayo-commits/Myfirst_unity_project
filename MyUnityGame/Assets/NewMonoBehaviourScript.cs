eb00e2fe6e3fbf594c3d3db5ddf41cefe1a754c0
lyyzzayo-commits <lyyzzayo@gmail.com>
2026-01-07 08:34:14 +0900

    Update NewMonoBehaviourScript.cs

--- MyUnityGame/Assets/NewMonoBehaviourScript.cs
+++ MyUnityGame/Assets/NewMonoBehaviourScript.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Time.timeScale = 0f;
     }
 
     // Update is called once per frame
